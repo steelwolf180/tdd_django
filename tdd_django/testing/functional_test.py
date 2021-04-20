81ca7742518ba05d50c159d6c7017fcec47f4f1f
steelwolf180 <steelwolf180@gmail.com>
2021-04-20 22:57:03 +0800

    fix: Changes to the assert code to be "successfully" instead of "Django".

--- tdd_django/testing/functional_test.py
+++ tdd_django/testing/functional_test.py
@@ -3,4 +3,4 @@ from selenium import webdriver
 browser = webdriver.Firefox(executable_path='../../geckodriver')
 browser.get('http://127.0.0.1:8000')
 
-assert 'Django' in browser.title
+assert 'successfully' in browser.title
